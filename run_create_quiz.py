981d0ec1cbe6049de5a1a4e3565858f3233af6f3
abbet <abbet.christian@gmail.com>
2024-08-13 11:04:25 +0200

    typo

--- run_create_quiz.py
+++ run_create_quiz.py
@@ -51,13 +51,13 @@ if __name__ == '__main__':
         prog='pybQuiz Creator',
         description='PybQuiz is a Python package designed to help you create and manage pub quizzes effortlessly',
     )
-    parser.add_argument('--cfg', default=None, action='store_const',
+    parser.add_argument('--cfg', default=None,
                         help='path to config file (default if None)')
-    parser.add_argument('--dirout', default="output", action='store_const',
+    parser.add_argument('--dirout', default="output", 
                         help='path to output directory for data generation (default is "output")')
-    parser.add_argument('--apitoken', default='config/apitoken.yml', action='store_const',
+    parser.add_argument('--apitoken', default='config/apitoken.yml', 
                         help='path to stored API tokens (default is "config/apitoken.yml")')
-    parser.add_argument('--googlecreds', default='config/credentials.json', action='store_const',
+    parser.add_argument('--googlecreds', default='config/credentials.json', 
                         help='path to stored Google credentials (default is "config/credentials.json")')
     args = parser.parse_args()
     
